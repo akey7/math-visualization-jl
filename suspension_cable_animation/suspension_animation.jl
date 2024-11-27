b35f594454c6e5cd08a905f4c0bfc58b93bbc54c
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-11-27 13:09:23 -0700

    Update suspension_animation.jl

--- suspension_cable_animation/suspension_animation.jl
+++ suspension_cable_animation/suspension_animation.jl
@@ -19,12 +19,12 @@ Vector of solutions to the tension equation.
 """
 function tension(;
     s::Float64,
-    x::Float64,
+    dist_from_support::Float64,
     w::Float64,
     guess_lower::Float64,
     guess_upper::Float64,
 )
-    partial(h) = h / w * sinh(w * x / h) - s
+    partial(h) = h / w * sinh(w * dist_from_support / h) - s
     fzeros(partial, guess_lower, guess_upper)
 end
 
@@ -56,7 +56,7 @@ function curve(;
     ys = zeros(Float64, length(xs))
     for (i, x) ∈ enumerate(xs)
         h = tension(
-            x = dist_from_support,
+            dist_from_support = dist_from_support,
             w = w,
             s = s,
             guess_lower = guess_lower,
@@ -65,7 +65,7 @@ function curve(;
         if length(h) < 1
             throw(
                 ErrorException(
-                    "No solutions for tension! s=$s, w=$w, x=$x, guess_lower=$guess_lower, guess_upper=$guess_upper",
+                    "No solutions for tension! s=$s, w=$w, dist_from_support=$dist_from_support, guess_lower=$guess_lower, guess_upper=$guess_upper",
                 ),
             )
         end
@@ -74,7 +74,9 @@ function curve(;
     ys
 end
 
-function render_frame(anim, max_dist_from_support::Float64, dist_from_support::Float64)
+function render_frame(anim::Plots.Animation, max_dist_from_support::Float64, dist_from_support::Float64)
+    width = 1080
+    height = 1920 / 2
     xs = collect(
         range(start = -max_dist_from_support, stop = max_dist_from_support, length = 100),
     )
@@ -83,10 +85,34 @@ function render_frame(anim, max_dist_from_support::Float64, dist_from_support::F
         dist_from_support = dist_from_support,
         w = 5.0,
         s = 25.0,
-        guess_lower = 30.0,
-        guess_upper = 60.0,
+        guess_lower = 0.0,
+        guess_upper = 100.0,
     )
-    plot(xs, ys, xlims = (-20.0, 25.0), ylims = (0.0, maximum(ys) * 1.1), legend = :none, linewidth = 3.0)
+    plot(
+        xs,
+        ys,
+        xlims = (-20.0, 25.0),
+        ylims = (0.0, maximum(ys) * 1.1),
+        legend = :none,
+        linewidth = 3.0,
+        height = height,
+        width = width,
+    )
     plot!([minimum(xs), minimum(xs)], [0.0, maximum(ys)], color = :red, linewidth = 7.0)
     plot!([maximum(xs), maximum(xs)], [0.0, maximum(ys)], color = :red, linewidth = 7.0)
+    frame(anim)
 end
+
+function render_curve_animation()
+    fps = 30
+    seconds = 5
+    n_frames = fps * seconds
+    anim = Animation()
+    dists_from_support = range(start = 15.0, stop = 20.0, length = n_frames)
+    for dist_from_support ∈ dists_from_support
+        render_frame(anim, 20.0, dist_from_support)
+    end
+    mp4(anim, "suspension_animation.mp4", fps = fps)
+end
+
+render_curve_animation()
