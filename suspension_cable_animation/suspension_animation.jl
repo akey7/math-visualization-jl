ff603651de93d36bb780a51f32eeb613ab6b4558
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-11-27 16:24:32 -0700

    Update suspension_animation.jl

--- suspension_cable_animation/suspension_animation.jl
+++ suspension_cable_animation/suspension_animation.jl
@@ -116,12 +116,12 @@ function render_frame(
     bottom_location = ys[Int64(length(ys) / 2)]
     bottom_annotation = @sprintf("%.1f", bottom_location)
     annotate!(0.0, bottom_location + 1.0, text("$bottom_annotation ft", :black, 16))
-    quiver!([0.0], [0.0], quiver = ([0.0], [bottom_location - 0.25]), color = :orange, linewidth = 7.0)
-    width_dimension = maximum(xs) - minimum(ys)
+    quiver!([0.0], [0.0], quiver = ([0.0], [bottom_location - 0.5]), color = :orange, linewidth = 7.0)
+    width_dimension = maximum(xs) - minimum(xs)
     width_annotation = @sprintf("%.1f", width_dimension)
     annotate!(0.0, maximum(ys), text("$width_annotation ft", :black, 16))
-    quiver!([2.0], [maximum(ys)], quiver = ([maximum(xs) - 2.75], [0.0]), color = :orange, linewidth = 7.0)
-    quiver!([-2.0], [maximum(ys)], quiver = ([minimum(xs) + 2.5], [0.0]), color = :orange, linewidth = 7.0)
+    quiver!([3.0], [maximum(ys)], quiver = ([maximum(xs) - 3.75], [0.0]), color = :orange, linewidth = 7.0)
+    quiver!([-3.0], [maximum(ys)], quiver = ([minimum(xs) + 3.75], [0.0]), color = :orange, linewidth = 7.0)
     frame(anim)
 end
 
