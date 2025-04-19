2a105d669b9ec314d8c5176d5f65c33892feaf7c
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-19 17:28:42 -0600

    Update example_5_1_2.jl

--- basic_vector_fields/example_5_1_2.jl
+++ basic_vector_fields/example_5_1_2.jl
@@ -1,17 +1,22 @@
 using PlotlyJS
 
-function f(t::Float64, a::Float64, x0::Float64 = 1.0, y0::Float64 = 1.0)
+function f(t::Float64, a::Float64; x0::Float64 = 1.0, y0::Float64 = 1.0)
     x = x0 * exp(a*t)
     y = y0 * exp(-t)
     (x, y)
 end
 
-points = [f(t, 0.0) for t ∈ range(0.0, 1.0, 10)]
-xs = [x for (x, _) ∈ points]
-ys = [y for (_, y) ∈ points]
-trace1 = scatter(x = xs, y = ys, mode = "lines")
+a = 0.0
+traces::Vector{AbstractTrace} = []
+for x0 in range(0.0, 1.0, 10)
+    points = [f(t, a; x0 = x0) for t ∈ range(0.0, 1.0, 10)]
+    xs = [x for (x, _) ∈ points]
+    ys = [y for (_, y) ∈ points]
+    trace = scatter(x = xs, y = ys, mode = "lines")
+    push!(traces, trace)
+end
 layout = Layout(width = 500, height = 500)
-p = plot([trace1], layout)
+p = plot(traces, layout)
 display(p)
 println("Press enter to exit")
 readline()
