da1a1f1d437de1634701dc691f79c8f9218511e7
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-20 08:33:34 -0600

    Update example_5_1_2.jl

--- basic_vector_fields/example_5_1_2.jl
+++ basic_vector_fields/example_5_1_2.jl
@@ -12,6 +12,13 @@ function portrait(a::Float64, r::Float64)
     for (x0, y0) ∈ zip(x0s, y0s)
         xs = x_eq.(ts, x0, a)
         ys = y_eq.(ts, y0)
+        trace_start = scatter(
+            x = [x0],
+            y = [y0],
+            mode = "markers",
+            marker = attr(color = "blue", size = 10),
+            showlegend = false,
+        )
         trace_line = scatter(
             x = xs,
             y = ys,
@@ -19,15 +26,16 @@ function portrait(a::Float64, r::Float64)
             line = attr(color = "black"),
             showlegend = false,
         )
-        trace_point = scatter(
-            x = [x0],
-            y = [y0],
+        trace_end = scatter(
+            x = [xs[end]],
+            y = [ys[end]],
             mode = "markers",
-            marker = attr(color = "blue", size = 10),
+            marker = attr(color = "red", size = 10),
             showlegend = false,
         )
+        push!(traces, trace_start)
         push!(traces, trace_line)
-        push!(traces, trace_point)
+        push!(traces, trace_end)
     end
     title = "a = $a"
     layout = Layout(title = title, width = 500, height = 500)
@@ -36,7 +44,6 @@ end
 
 display(portrait(-2.0, 0.1))
 display(portrait(-1.0, 1.0))
-# display(portrait(0.0, 1.0, [0.0, π/4, π/2, π, 3π/4, 5π/4, 3π/2, 7π/4]))
 display(portrait(1.0, 1.0))
 println("Press enter to continue")
 readline()
