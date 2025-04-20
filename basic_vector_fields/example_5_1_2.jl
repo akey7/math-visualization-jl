f1dbe549d1e12468380200a7bcdbb240dbf7712a
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-20 08:01:59 -0600

    Update example_5_1_2.jl

--- basic_vector_fields/example_5_1_2.jl
+++ basic_vector_fields/example_5_1_2.jl
@@ -11,22 +11,30 @@ function portrait(a::Float64, r::Float64, rotations::Int64)
     for (x0, y0) ∈ zip(x0s, y0s)
         xs = x_eq.(ts, x0, a)
         ys = y_eq.(ts, y0)
-        trace = scatter(
+        trace_line = scatter(
             x = xs,
             y = ys,
             mode = "lines",
             line = attr(color = "black"),
             showlegend = false,
         )
-        push!(traces, trace)
+        trace_point = scatter(
+            x = [x0],
+            y = [y0],
+            mode = "markers",
+            marker = attr(color = "blue", size = 10),
+            showlegend = false,
+        )
+        push!(traces, trace_line)
+        push!(traces, trace_point)
     end
     layout = Layout(width = 500, height = 500)
     plot(traces, layout)
 end
 
-display(portrait(-2.0, 1.0, 6))
-display(portrait(-1.0, 1.0, 6))
-display(portrait(0.0, 1.0, 6))
-display(portrait(1.0, 1.0, 6))
+display(portrait(-2.0, 0.1, 12))
+# display(portrait(-1.0, 1.0, 6))
+# display(portrait(0.0, 1.0, 6))
+# display(portrait(1.0, 1.0, 6))
 println("Press enter to continue")
 readline()
