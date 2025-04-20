cbf33e250ae7cf2e0b261860967358b01c597533
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-20 08:19:42 -0600

    Update example_5_1_2.jl

--- basic_vector_fields/example_5_1_2.jl
+++ basic_vector_fields/example_5_1_2.jl
@@ -3,11 +3,11 @@ using PlotlyJS
 x_eq(t, x0, a) = x0 * exp(a * t)
 y_eq(t, y0) = y0 * exp(-t)
 
-function portrait(a::Float64, r::Float64, rotations::Int64)
+function portrait(a::Float64, r::Float64, angles::Vector{Float64})
     traces::Vector{GenericTrace} = []
-    x0s = [r * cos(θ) for θ ∈ range(0.0, 2π, rotations)]
-    y0s = [r * sin(θ) for θ ∈ range(0.0, 2π, rotations)]
-    ts = range(0.0, 1.0, length = 10)
+    x0s = [r * cos(θ) for θ ∈ angles]
+    y0s = [r * sin(θ) for θ ∈ angles]
+    ts = range(0.0, 2.0, length = 10)
     for (x0, y0) ∈ zip(x0s, y0s)
         xs = x_eq.(ts, x0, a)
         ys = y_eq.(ts, y0)
@@ -32,7 +32,7 @@ function portrait(a::Float64, r::Float64, rotations::Int64)
     plot(traces, layout)
 end
 
-display(portrait(-2.0, 0.1, 12))
+display(portrait(-2.0, 0.1, [0.0, π/4, π/2, π, 3π/4, 5π/4, 3π/2, 7π/4]))
 # display(portrait(-1.0, 1.0, 6))
 # display(portrait(0.0, 1.0, 6))
 # display(portrait(1.0, 1.0, 6))
