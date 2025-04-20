a16428a0a72462539cd1672d3c4223497a74a546
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-19 18:43:45 -0600

    Update example_5_1_2.jl

--- basic_vector_fields/example_5_1_2.jl
+++ basic_vector_fields/example_5_1_2.jl
@@ -1,6 +1,6 @@
 using PlotlyJS
 
-function f(t::Float64, a::Float64; x0::Float64 = 1.0, y0::Float64 = 1.0)
+function f(t::Float64; a::Float64, x0::Float64 = 1.0, y0::Float64 = 1.0)
     x = x0 * exp(a*t)
     y = y0 * exp(-t)
     (x, y)
@@ -8,20 +8,23 @@ end
 
 a = -2.0
 traces::Vector{AbstractTrace} = []
-xs::Vector{Float64} = []
-ys::Vector{Float64} = []
-for t ∈ range(-1.0, 1.0, 100)
-    (x, y) = f(t, a)
-    push!(xs, x)
-    push!(ys, y)
+for x0 ∈ range(-1.0, 1.0, 5)
+    xs::Vector{Float64} = []
+    ys::Vector{Float64} = []
+    for t ∈ range(-1.0, 1.0, 100)
+        (x, y) = f(t; a = a, x0 = x0)
+        push!(xs, x)
+        push!(ys, y)
+    end
+    trace = scatter(
+        x = xs,
+        y = ys,
+        mode = "lines",
+    )
+    push!(traces, trace)
 end
-trace1 = scatter(
-    x = xs,
-    y = ys,
-    mode = "lines",
-)
 layout = Layout(width = 500, height = 500)
-p = plot([trace1], layout)
+p = plot(traces, layout)
 display(p)
 println("Press enter to exit")
 readline()
