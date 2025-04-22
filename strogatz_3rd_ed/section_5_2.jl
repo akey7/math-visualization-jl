ae3bb93a207588c25c6cb0ba2f5e3ed9c705abd8
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-21 20:24:36 -0600

    Update section_5_2.jl

--- strogatz_3rd_ed/section_5_2.jl
+++ strogatz_3rd_ed/section_5_2.jl
@@ -41,47 +41,18 @@ x_eq(t) = c1_c2[1] * eig.vectors[1, 1] * exp(eig.values[1] * t) + c1_c2[2] * eig
 y_eq(t) = c1_c2[1] * eig.vectors[2, 1] * exp(eig.values[1] * t) + c1_c2[2] * eig.vectors[2, 2] * exp(eig.values[2] * t)
 
 #####################################################
-# PLOTTING FUNCTION                                 #
+# DISPALY A PLOT                                    #
 #####################################################
 
-function portrait(r::Float64)
-    traces::Vector{GenericTrace} = []
-    angles = [0.0, π/4, π/2, π, 3π/4, 5π/4, 3π/2, 7π/4]
-    x0s = [r * cos(θ) for θ ∈ angles]
-    y0s = [r * sin(θ) for θ ∈ angles]
-    ts = range(0.0, 2.0, length = 10)
-    for (i, (x0, y0)) ∈ enumerate(zip(x0s, y0s))
-        xs = x_eq.(ts)
-        ys = y_eq.(ts)
-        showlegend = i == 1
-        trace_start = scatter(
-            x = [x0],
-            y = [y0],
-            mode = "markers",
-            marker = attr(color = "blue", size = 10),
-            name = "start",
-            showlegend = showlegend,
-        )
-        trace_line = scatter(
-            x = xs,
-            y = ys,
-            mode = "lines",
-            line = attr(color = "black"),
-            name = "path",
-            showlegend = showlegend,
-        )
-        trace_end = scatter(
-            x = [xs[end]],
-            y = [ys[end]],
-            mode = "markers",
-            marker = attr(color = "red", size = 10),
-            name = "end",
-            showlegend = showlegend,
-        )
-        push!(traces, trace_start)
-        push!(traces, trace_line)
-        push!(traces, trace_end)
-    end
-    layout = Layout(width = 500, height = 500)
-    plot(traces, layout)
-end
+cs = range(-10.0, 10.0, 100)
+xs1 = cs .* eig.vectors[1, 1]
+ys1 = cs .* eig.vectors[2, 1]
+xs2 = cs .* eig.vectors[1, 2]
+ys2 = cs .* eig.vectors[2, 2]
+trace1 = scatter(x = xs1, y = ys1, mode = "lines")
+trace2 = scatter(x = xs2, y = ys2, mode = "lines")
+layout = Layout(width = 500, height = 500)
+p = plot([trace1, trace2], layout)
+PlotlyJS.display(p)
+println("Press enter to exit")
+readline()
