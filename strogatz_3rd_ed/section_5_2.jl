b7f5fa4c413421ea5f0fb34502db377def9a5ea0
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-21 19:41:22 -0600

    Update section_5_2.jl

--- strogatz_3rd_ed/section_5_2.jl
+++ strogatz_3rd_ed/section_5_2.jl
@@ -4,6 +4,14 @@ using PlotlyJS
 A = [1.0 1.0; 4.0 -2.0]
 eig = eigen(A)
 println("Eigenvalues")
-println(eig.values)
+display(eig.values)
 println("Eigenvectors")
-println(eig.vectors)
+display(eig.vectors)
+
+for i ∈ 1:2
+    v = eig.vectors[:, i]
+    λ = eig.values[i]
+    lhs = A * v
+    rhs = λ * v 
+    println(all(lhs .≈ rhs))
+end
