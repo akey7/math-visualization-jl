e8038a19b5f7130aa9942c0ed1be6da338613e59
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-22 10:03:22 -0600

    Update section_5_2.jl

--- strogatz_3rd_ed/section_5_2.jl
+++ strogatz_3rd_ed/section_5_2.jl
@@ -11,9 +11,22 @@ function solve_for_ic(A::Matrix{Float64}, ics::Vector{Float64})
     display(ics)
     eig = eigen(A)
     println("Eigenvalues")
-    display(eig.values)
-    println("Eigenvectors")
-    display(eig.vectors)
+    λ = eig.values
+    display(λ)
+    println("Normalized eigenvectors")
+    v = eig.vectors
+    display(v)
+    c = eig.vectors \ ics
+    println("c1 and c2")
+    display(c)
+    x_eq(t::Float64) = c[1]*v[1,1]*exp(λ[1]*t) + c[2]*v[1,2]*exp(λ[2]*t)
+    y_eq(t::Float64) = c[1]*v[1,2]*exp(λ[1]*t) + c[2]*v[2,2]*exp(λ[2]*t)
+    x_eq, y_eq
 end
 
-solve_for_ic([1.0 1.0; 4.0 -2.0], [2.0; -3.0])
+A = [1.0 1.0; 4.0 -2.0]
+for x ∈ [2.0]
+    for y ∈ [-3.0]
+        x_eq, y_eq = solve_for_ic(A, [x, y])
+    end
+end
