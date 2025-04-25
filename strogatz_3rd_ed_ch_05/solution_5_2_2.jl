fb433eb2978431179822e85ebd615cd7aa5c7420
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-25 17:27:26 -0600

    Solving with DifferentialEquations.jl

--- strogatz_3rd_ed_ch_05/solution_5_2_2.jl
+++ strogatz_3rd_ed_ch_05/solution_5_2_2.jl
@@ -1,23 +1,20 @@
 using LinearAlgebra
+using DifferentialEquations
 using PlotlyJS
 
-function f(t::Float64, c1::Float64 = 1.0, c2::Float64 = 1.0)
-    @. c1 * exp(t) * [cos(t); sin(t)] + c2 * exp(t) * [-sin(t); cos(t)] 
+function solve_system(A::Matrix{Float64}, x0::Vector{Float64}, tspan::Tuple{Float64,Float64})
+    f(x, p, t) = A * x
+    prob = ODEProblem(f, x0, tspan)
+    solve(prob, Tsit5())
 end
 
-function portrait()
-    ts = range(0.0, 1.0, 10)
-    values = f.(ts)
-    xs = [x for (x, _) ∈ values]
-    ys = [y for (_, y) ∈ values]
+function portrait(A::Matrix{Float64}, x0::Vector{Float64}, tspan::Tuple{Float64,Float64})
+    sol = solve_system(A, x0, tspan)
+    xs = sol[1, :]
+    ys = sol[2, :]
     traces::Vector{GenericTrace} = []
-    trace_path = scatter(
-        x = xs,
-        y = ys,
-        mode = "lines",
-        line = attr(color = "black"),
-        name = "path",
-    )
+    trace_path =
+        scatter(x = xs, y = ys, mode = "lines", line = attr(color = "black"), name = "path")
     trace_start = scatter(
         x = [xs[1]],
         y = [ys[1]],
@@ -39,6 +36,6 @@ function portrait()
     plot(traces, layout)
 end
 
-display(portrait())
+display(portrait([0.0 1.0; -2.0 -1.0], [1.0, 0.0], (0.0, 1.0)))
 println("Press enter to exit...")
 readline()
