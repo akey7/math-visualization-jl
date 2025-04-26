29b67a91c4fb4621b1f28c9c5c0b6596528905d9
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-26 14:25:44 -0600

    Create inspired_by_fig_5_3_2.jl

---
+++ strogatz_3rd_ed_ch_05/inspired_by_fig_5_3_2.jl
@@ -0,0 +1,27 @@
+using LinearAlgebra
+using PlotlyJS
+
+function solve_for_ics(A::Matrix{Float64}, ics::Vector{Float64})
+    println("#####################################################")
+    println("# BEGIN SOLVE                                       #")
+    println("#####################################################")
+    println("A:")
+    display(A)
+    println("Initial conditions:")
+    display(ics)
+    eig = eigen(A)
+    println("Eigenvalues")
+    λ = eig.values
+    display(λ)
+    println("Normalized eigenvectors")
+    v = eig.vectors
+    display(v)
+    c = eig.vectors \ ics
+    println("c1 and c2")
+    display(c)
+    x_eq(t::Float64) = c[1]*v[1, 1]*exp(λ[1]*t) + c[2]*v[1, 2]*exp(λ[2]*t)
+    y_eq(t::Float64) = c[1]*v[1, 2]*exp(λ[1]*t) + c[2]*v[2, 2]*exp(λ[2]*t)
+    x_eq, y_eq
+end
+
+
