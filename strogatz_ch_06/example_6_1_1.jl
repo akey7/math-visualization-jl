c3558d3c6f3176e1f1f116059417e81e8df0bd26
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-27 13:18:51 -0600

    Found zeros of problem.

--- strogatz_ch_06/example_6_1_1.jl
+++ strogatz_ch_06/example_6_1_1.jl
@@ -1,3 +1,13 @@
-using NonelinearSolve
+using NonlinearSolve
 
+function f(x, p)
+    [
+        x[1] + exp(-x[2]),
+        -x[2]
+    ]
+end
 
+initial_x = [0.5, 0.5]
+prob = NonlinearProblem(f, initial_x)
+sol = solve(prob, NewtonRaphson())
+println("Solution: ", sol.u)
