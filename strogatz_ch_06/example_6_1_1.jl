092700c2a13a0004752a0a70f98ff9c2df446031
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-27 14:03:33 -0600

    Issue #16
    Refactor functions for reuse.

--- strogatz_ch_06/example_6_1_1.jl
+++ strogatz_ch_06/example_6_1_1.jl
@@ -1,13 +1,29 @@
 using NonlinearSolve
 
-function f(x, p)
-    [
-        x[1] + exp(-x[2]),
-        -x[2]
-    ]
+########################################################
+# SYSTEM OF EQUATIONS                                  #
+########################################################
+
+f(x) = x[1] + exp(-x[2])
+g(x) = -x[2]
+
+########################################################
+# FIND FIXED POINT                                     #
+########################################################
+
+function system_of_eqs(x, p)
+    [f(x), g(x)]
 end
 
 initial_x = [0.5, 0.5]
-prob = NonlinearProblem(f, initial_x)
+prob = NonlinearProblem(system_of_eqs, initial_x)
 sol = solve(prob, NewtonRaphson())
-println("Solution: ", sol.u)
+println("Fixed point: ", sol.u)
+
+########################################################
+# FIND NULLCLINES GRAPHICALLY                          #
+########################################################
+
+x_vals = range(-10.0, 10.0, 100)
+y_vals = range(-10.0, 10.0, 100)
+contour = []
