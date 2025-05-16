0cbdf2552b4d032f975ac65501d04cdbe664a6da
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-16 07:27:52 -0600

    Issue #46
    Calculation of symbolic Jacobian is successful!

--- strogatz_ch_07/ex_7_3_2_symbolics.jl
+++ strogatz_ch_07/ex_7_3_2_symbolics.jl
@@ -1,3 +1,9 @@
 using Symbolics
 
-
+@variables a b x y
+x_dot = -x + a*y + x^2*y
+y_dot = b - a*y - x^2*y 
+F = [x_dot, y_dot]
+vars = [x, y]
+J = Symbolics.jacobian(F, vars; simplify = true)
+display(J)
