fae57edea01ce1bd231052f2cb824eaae4ea243b
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-24 16:59:15 -0600

    Issue #51
    Obtain access to widgets from the interface builder.

--- foxes_and_rabbits/foxes_and_rabbits.jl
+++ foxes_and_rabbits/foxes_and_rabbits.jl
@@ -8,6 +8,20 @@ gr()
 b_filename = joinpath("foxes_and_rabbits", "foxes_and_rabbits_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
+button_update = b["button_update"]
+button_reset = b["button_reset"]
+scale_initial_fox_pop = b["scale_initial_fox_pop"]
+scale_initial_rabbit_pop = b["scale_initial_rabbit_pop"]
+scale_rabbit_pop_growth_rate = b["scale_rabbit_pop_growth_rate"]
+scale_rabbit_eaten_rate = b["scale_rabbit_eaten_rate"]
+scale_fox_pop_depletion_rate = b["scale_fox_pop_depletion_rate"]
+scale_fox_birth_rate = b["scale_fox_birth_rate"]
+frame_timeseries = b["frame_timeseries"]
+frame_phase_portrait = b["frame_phase_portrait"]
+canvas_timeseries = GtkCanvas()
+canvas_phase_portrait = GtkCanvas()
+push!(frame_timeseries, canvas_timeseries)
+push!(frame_phase_portrait, canvas_phase_portrait)
 
 showall(win)
 # button_update_clicked(button_update)  # Get the initial default values from UI
