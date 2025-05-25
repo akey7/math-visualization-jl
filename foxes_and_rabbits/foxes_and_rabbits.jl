e42f9c5383eb170d4b607396d0fb49bd0f540db7
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-24 20:04:34 -0600

    Issue #51
    Getting ready to solve the DEs.

--- foxes_and_rabbits/foxes_and_rabbits.jl
+++ foxes_and_rabbits/foxes_and_rabbits.jl
@@ -2,8 +2,20 @@ using Base64
 using Gtk
 using Plots
 using Cairo
+using DifferentialEquations
 
 gr()
+
+function calc_trajectories(
+    initital_fox_pop,
+    initial_rabbit_pop,
+    rabbit_pop_growth_rate,
+    rabbit_eaten_rate,
+    fox_pop_depletion_rate,
+    fox_birth_rate,
+)
+
+end
 
 b_filename = joinpath("foxes_and_rabbits", "foxes_and_rabbits_ui.glade")
 b = GtkBuilder(filename = b_filename)
@@ -24,7 +36,20 @@ push!(frame_timeseries, canvas_timeseries)
 push!(frame_phase_portrait, canvas_phase_portrait)
 
 @guarded function button_update_clicked(widget, others...)
-    println("Clicked!")
+    initital_fox_pop = GAccessor.value(scale_initial_fox_pop)
+    initial_rabbit_pop = GAccessor.value(scale_initial_rabbit_pop)
+    rabbit_pop_growth_rate = GAccessor.value(scale_rabbit_pop_growth_rate)
+    rabbit_eaten_rate = GAccessor.value(scale_rabbit_eaten_rate)
+    fox_pop_depletion_rate = GAccessor.value(scale_fox_pop_depletion_rate)
+    fox_birth_rate = GAccessor.value(scale_fox_birth_rate)
+    calc_trajectories(
+        initital_fox_pop,
+        initial_rabbit_pop,
+        rabbit_pop_growth_rate,
+        rabbit_eaten_rate,
+        fox_pop_depletion_rate,
+        fox_birth_rate,
+    )
 end
 
 signal_connect(button_update_clicked, button_update, "clicked")
