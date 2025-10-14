a0dcf7130ceb0f4af7f7e70e81a32e5831b43fa1
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 10:49:54 -0600

    Update markov_chain.jl

--- markov_chain_basic/markov_chain.jl
+++ markov_chain_basic/markov_chain.jl
@@ -5,6 +5,15 @@ transition_matrix = [0.5 0.1 0.4; 0.1 0.6 0.3; 0.7 0.15 0.15]
 function run_chain(n_steps, initial_state)
     random_nums = rand(Float64, n_steps)
     steps = zeros(Float64, n_steps)
+    steps[1] = initial_state
+    for i in eachindex(steps)
+        prev = steps[i-1]
+        prev_probs = transition_matrix[prev,:]
+        random_num = random_nums[i-1]
+        if random_num < prev_probs[1]
+            steps[i] = 1
+        end
+    end
     return steps[1:100]
 end
 println(run_chain(10_000, 1))
