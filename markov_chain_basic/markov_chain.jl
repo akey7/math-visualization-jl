43cabe583d4e590714d50e740bf2330034cf50a8
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 10:58:25 -0600

    Issue #57
    First pass at making a chain.

--- markov_chain_basic/markov_chain.jl
+++ markov_chain_basic/markov_chain.jl
@@ -3,17 +3,21 @@ using Random
 Random.seed!(123)
 transition_matrix = [0.5 0.1 0.4; 0.1 0.6 0.3; 0.7 0.15 0.15]
 function run_chain(n_steps, initial_state)
-    random_nums = rand(Float64, n_steps)
-    steps = zeros(Float64, n_steps)
+    random_nums = rand(n_steps)
+    steps = zeros(Int64, n_steps)
     steps[1] = initial_state
-    for i in eachindex(steps)
-        prev = steps[i-1]
-        prev_probs = transition_matrix[prev,:]
+    for i in eachindex(steps)[2:end]
+        prev_state = steps[i-1]
+        prev_probs = transition_matrix[prev_state,:]
         random_num = random_nums[i-1]
         if random_num < prev_probs[1]
             steps[i] = 1
+        elseif random_num < (prev_probs[1] + prev_probs[2])
+            steps[i] = 2
+        else
+            steps[i] = 3
         end
     end
-    return steps[1:100]
+    return steps
 end
-println(run_chain(10_000, 1))
+println(run_chain(100, 1.0))
