dc0de71abcfca325c07395698d47d7a366af4caf
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 08:36:13 -0600

    Issue #57
    Beginnings of random generation.

--- markov_chain_basic/markov_chain.jl
+++ markov_chain_basic/markov_chain.jl
@@ -1,3 +1,10 @@
-transition_matrix = [0.5 0.1 0.4; 0.1 0.6 0.3; 0.7 0.15 0.15]
+using Random
 
-println(transition_matrix[1,1])
+Random.seed!(123)
+transition_matrix = [0.5 0.1 0.4; 0.1 0.6 0.3; 0.7 0.15 0.15]
+function run_chain(n_steps, initial_state)
+    random_nums = rand(Float64, n_steps)
+    steps = zeros(Float64, n_steps)
+    return steps[1:100]
+end
+println(run_chain(10_000, 1))
