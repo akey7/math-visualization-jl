e32ae3439ff8b3f2fce056dc1f7700653166af76
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-10-14 11:04:52 -0600

    Issue #57
    Count states

--- markov_chain_basic/markov_chain.jl
+++ markov_chain_basic/markov_chain.jl
@@ -4,20 +4,29 @@ Random.seed!(123)
 transition_matrix = [0.5 0.1 0.4; 0.1 0.6 0.3; 0.7 0.15 0.15]
 function run_chain(n_steps, initial_state)
     random_nums = rand(n_steps)
-    steps = zeros(Int64, n_steps)
-    steps[1] = initial_state
-    for i in eachindex(steps)[2:end]
-        prev_state = steps[i-1]
-        prev_probs = transition_matrix[prev_state,:]
+    chain = zeros(Int64, n_steps)
+    chain[1] = initial_state
+    for i in eachindex(chain)[2:end]
+        prev_state = chain[i-1]
+        prev_probs = transition_matrix[prev_state, :]
         random_num = random_nums[i-1]
         if random_num < prev_probs[1]
-            steps[i] = 1
+            chain[i] = 1
         elseif random_num < (prev_probs[1] + prev_probs[2])
-            steps[i] = 2
+            chain[i] = 2
         else
-            steps[i] = 3
+            chain[i] = 3
         end
     end
-    return steps
+    return chain
 end
-println(run_chain(100, 1.0))
+function count_states(chain)
+    counts = zeros(Int64, 3)
+    for state in chain
+        counts[state] += 1
+    end
+    fractions = counts ./ length(chain)
+    return fractions
+end
+single_chain = run_chain(10_000, 1)
+println(count_states(single_chain))
